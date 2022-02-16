2349a2beb4eb636c7ec75698aee7ab6cebae405a
B. Shyam Sundar <shyamsundar.b@novactech.in>
2022-02-16 17:43:45 +0530

    initial commmit

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		},
+		target: '#svelte'
+	}
+};
+
+export default config;
